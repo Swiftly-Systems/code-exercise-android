c770d8d6b244d67724dbd992a0157bbfb0eb25b7
Javier.Gonzalez <javglex@gmail.com>
2021-02-26 20:23:44 -0800

    Removed unneeded DI class.
    Removed unneeded scope from MainActivity

--- app/src/main/java/com/swiftly/codingexersicejavi/presentation/MainActivity.kt
+++ app/src/main/java/com/swiftly/codingexersicejavi/presentation/MainActivity.kt
@@ -4,27 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.ViewModelProviders
 import com.swiftly.codingexersicejavi.R
 import com.swiftly.codingexersicejavi.framework.ManagerSpecialsViewModel
-import com.swiftly.codingexersicejavi.framework.di.ApplicationModule
-import com.swiftly.codingexersicejavi.framework.di.DaggerActivityComponent
-import com.swiftly.codingexersicejavi.framework.network.ManagerSpecialService
-import com.swiftly.core.data.Resource
-import com.swiftly.core.data.ManagerSpecial
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var mainHandler: Handler
     private lateinit var viewModel: ManagerSpecialsViewModel
-    private var job = Job()
-    private val scope = CoroutineScope(Dispatchers.Main + job)
     private val REFRESH_RATE: Long = 10000
     /**
      * For simplicty will be running a loop during the main activity lifecycle when the user is
@@ -64,10 +51,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun fetchSpecials(){
-        scope.launch {
-            viewModel.getSpecials()
-        }
-
+        viewModel.getSpecials()
     }
 
 }
